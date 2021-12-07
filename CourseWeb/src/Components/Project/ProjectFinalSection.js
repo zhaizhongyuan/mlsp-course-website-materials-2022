62221244d19a5ad208c36bf14a79bd7ba8ca5df4
马英浩 <nicolaus625@MacBook-Pro-16-inch-2021.local>
2021-12-07 00:18:27 -0500

    change the final project page limit

--- CourseWeb/src/Components/Project/ProjectFinalSection.js
+++ CourseWeb/src/Components/Project/ProjectFinalSection.js
@@ -15,7 +15,7 @@ export default function ProjectFinalSection() {
 				<a href="https://nips.cc/Conferences/2020/PaperInformation/StyleFiles" className="text-indigo-600">
 					NeurIPS 2020 style template&nbsp;
 				</a>
-				and not be longer than two pages. References do not count towards your two-page limit. Your team's midterm report should be submitted as a PDF using Canvas. Each group only needs to submit one copy of the report. You can submit your report as many times as you need before the deadline. Please name your PDF "final-{"{group number}"}.pdf". Your report will be graded with the following things in mind:
+				and not be less than two pages. References and appendix do not count towards your limit. Your team's midterm report should be submitted as a PDF using Canvas. Each group only needs to submit one copy of the report. You can submit your report as many times as you need before the deadline. Please name your PDF "final-{"{group number}"}.pdf". Your report will be graded with the following things in mind:
 			</p>
 			<ul className="list-disc text-xl leading-7 text-gray-500 mb-4 pl-12">
 				<li>
